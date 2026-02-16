fde2904586f4bab98859ebdeb1d94887c360a75d
Cuma Ali Kesici <cumakessci@gmail.com>
2026-02-16 10:26:05 +0100

    Initial Commit

---
+++ MetalCanvasTest/MetalCanvasTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalCanvasTestApp.swift
+//  MetalCanvasTest
+//
+//  Created by Cuma Ali Kesici on 16.02.26.
+//
+
+import SwiftUI
+
+@main
+struct MetalCanvasTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
